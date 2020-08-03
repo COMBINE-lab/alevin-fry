427707662e136458b0357623d9b9b6f573fd0c0b
Rob Patro <rob-p@users.noreply.github.com>
2020-08-03 12:49:26 -0400

    update conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ author = 'Avi Srivastava, Rob Patro'
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
