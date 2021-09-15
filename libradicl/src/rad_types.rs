3c350ae5de4b08743421f0f5aa2b03b0fb56bfab
Rob Patro <rob@newton>
2021-09-15 10:40:11 -0400

    add missing file

---
+++ libradicl/src/rad_types.rs
@@ -0,0 +1,548 @@
+/*
+ * Copyright (c) 2020-2021 Rob Patro, Avi Srivastava, Hirak Sarkar, Dongze He, Mohsen Zakeri.
+ *
+ * This file is part of alevin-fry
+ * (see https://github.com/COMBINE-lab/alevin-fry).
+ *
+ * License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause
+ */
+
+use crate as libradicl;
+
+use self::libradicl::utils;
+use bio_types::strand::*;
+use num::cast::AsPrimitive;
+use rust_htslib::bam::HeaderView;
+use scroll::Pread;
+use std::io::Write;
+use std::io::{Cursor, Read};
+use std::mem;
+
+pub struct RadHeader {
+    pub is_paired: u8,
+    pub ref_count: u64,
+    pub ref_names: Vec<String>,
+    pub num_chunks: u64,
+}
+
+pub struct TagDesc {
+    pub name: String,
+    pub typeid: u8,
+}
+
+pub struct TagSection {
+    pub tags: Vec<TagDesc>,
+}
+
+// The below are currently hard-coded
+// until we decide how to solve this
+// generally
+#[derive(Debug)]
+pub struct FileTags {
+    pub bclen: u16,
+    pub umilen: u16,
+}
+#[derive(Debug)]
+pub struct ReadRecord {
+    pub bc: u64,
+    pub umi: u64,
+    pub dirs: Vec<bool>,
+    pub refs: Vec<u32>,
+}
+#[derive(Debug)]
+pub struct Chunk {
+    pub nbytes: u32,
+    pub nrec: u32,
+    pub reads: Vec<ReadRecord>,
+}
+
+#[derive(Debug)]
+pub struct CorrectedCbChunk {
+    pub(crate) remaining_records: u32,
+    pub(crate) corrected_bc: u64,
+    pub(crate) nrec: u32,
+    pub(crate) data: Cursor<Vec<u8>>, /*,
+                                      umis: Vec<u64>,
+                                      ref_offsets: Vec<u32>,
+                                      ref_ids: Vec<u32>,
+                                      */
+}
+
+#[derive(Copy, Clone)]
+pub enum RadIntId {
+    U8,
+    U16,
+    U32,
+    U64,
+}
+
+pub trait PrimitiveInteger:
+    AsPrimitive<u8>
+    + AsPrimitive<u16>
+    + AsPrimitive<u32>
+    + AsPrimitive<u64>
+    + AsPrimitive<usize>
+    + AsPrimitive<i8>
+    + AsPrimitive<i16>
+    + AsPrimitive<i32>
+    + AsPrimitive<i64>
+    + AsPrimitive<isize>
+{
+}
+
+impl<
+        T: AsPrimitive<u8>
+            + AsPrimitive<u16>
+            + AsPrimitive<u32>
+            + AsPrimitive<u64>
+            + AsPrimitive<usize>
+            + AsPrimitive<i8>
+            + AsPrimitive<i16>
+            + AsPrimitive<i32>
+            + AsPrimitive<i64>
+            + AsPrimitive<isize>,
+    > PrimitiveInteger for T
+{
+}
+
+impl RadIntId {
+    pub fn bytes_for_type(&self) -> usize {
+        match self {
+            Self::U8 => std::mem::size_of::<u8>(),
+            Self::U16 => std::mem::size_of::<u16>(),
+            Self::U32 => std::mem::size_of::<u32>(),
+            Self::U64 => std::mem::size_of::<u64>(),
+        }
+    }
+
+    /// Based on the variant of the current enum, write the value `v`
+    /// out using `owrite`.  Here, `v` is bound to be some primitive
+    /// integer type.  It is the responsibility of the caller to ensure
+    /// that, if `v` is wider than the enum type on which this function
+    /// is called, no important information is lost by discarding the higher
+    /// order bits.
+    pub fn write_to<T: PrimitiveInteger, U: Write>(
+        &self,
+        v: T,
+        owriter: &mut U,
+    ) -> std::io::Result<()> {
+        match self {
+            Self::U8 => {
+                let vo: u8 = v.as_();
+                owriter.write_all(&vo.to_le_bytes())
+            }
+            Self::U16 => {
+                let vo: u16 = v.as_();
+                owriter.write_all(&vo.to_le_bytes())
+            }
+            Self::U32 => {
+                let vo: u32 = v.as_();
+                owriter.write_all(&vo.to_le_bytes())
+            }
+            Self::U64 => {
+                let vo: u64 = v.as_();
+                owriter.write_all(&vo.to_le_bytes())
+            }
+        }
+    }
+}
+
+pub struct ChunkConfig {
+    pub num_chunks: u64,
+    pub bc_type: u8,
+    pub umi_type: u8,
+}
+
+#[derive(Copy, Clone)]
+pub enum RadType {
+    Bool,
+    U8,
+    U16,
+    U32,
+    U64,
+    F32,
+    F64,
+}
+
+pub fn encode_type_tag(type_tag: RadType) -> Option<u8> {
+    match type_tag {
+        RadType::Bool => Some(0),
+        RadType::U8 => Some(1),
+        RadType::U16 => Some(2),
+        RadType::U32 => Some(3),
+        RadType::U64 => Some(4),
+        RadType::F32 => Some(5),
+        RadType::F64 => Some(6),
+        //_ => None,
+    }
+}
+
+pub fn decode_int_type_tag(type_id: u8) -> Option<RadIntId> {
+    match type_id {
+        1 => Some(RadIntId::U8),
+        2 => Some(RadIntId::U16),
+        3 => Some(RadIntId::U32),
+        4 => Some(RadIntId::U64),
+        _ => None,
+    }
+}
+
+fn read_into_u64<T: Read>(reader: &mut T, rt: &RadIntId) -> u64 {
+    let mut rbuf = [0u8; 8];
+    let v: u64;
+    match rt {
+        RadIntId::U8 => {
+            reader.read_exact(&mut rbuf[0..1]).unwrap();
+            v = rbuf.pread::<u8>(0).unwrap() as u64;
+        }
+        RadIntId::U16 => {
+            reader.read_exact(&mut rbuf[0..2]).unwrap();
+            v = rbuf.pread::<u16>(0).unwrap() as u64;
+        }
+        RadIntId::U32 => {
+            reader.read_exact(&mut rbuf[0..4]).unwrap();
+            v = rbuf.pread::<u32>(0).unwrap() as u64;
+        }
+        RadIntId::U64 => {
+            reader.read_exact(&mut rbuf[0..8]).unwrap();
+            v = rbuf.pread::<u64>(0).unwrap();
+        }
+    }
+    v
+}
+
+impl ReadRecord {
+    pub fn is_empty(&self) -> bool {
+        self.refs.is_empty()
+    }
+
+    pub fn from_bytes<T: Read>(reader: &mut T, bct: &RadIntId, umit: &RadIntId) -> Self {
+        let mut rbuf = [0u8; 255];
+
+        reader.read_exact(&mut rbuf[0..4]).unwrap();
+        let na = rbuf.pread::<u32>(0).unwrap();
+        let bc = read_into_u64(reader, bct);
+        let umi = read_into_u64(reader, umit);
+
+        let mut rec = Self {
+            bc,
+            umi,
+            dirs: Vec::with_capacity(na as usize),
+            refs: Vec::with_capacity(na as usize),
+        };
+
+        //println!("number of records : {:?}",na);
+
+        for _ in 0..(na as usize) {
+            reader.read_exact(&mut rbuf[0..4]).unwrap();
+            let v = rbuf.pread::<u32>(0).unwrap();
+            let dir = (v & utils::MASK_LOWER_31_U32) != 0;
+            rec.dirs.push(dir);
+            rec.refs.push(v & utils::MASK_TOP_BIT_U32);
+        }
+
+        rec
+    }
+
+    pub fn from_bytes_record_header<T: Read>(
+        reader: &mut T,
+        bct: &RadIntId,
+        umit: &RadIntId,
+    ) -> (u64, u64, u32) {
+        let mut rbuf = [0u8; 4];
+        reader.read_exact(&mut rbuf).unwrap();
+        let na = u32::from_le_bytes(rbuf); //.pread::<u32>(0).unwrap();
+        let bc = read_into_u64(reader, bct);
+        let umi = read_into_u64(reader, umit);
+        (bc, umi, na)
+    }
+
+    pub fn from_bytes_with_header_keep_ori<T: Read>(
+        reader: &mut T,
+        bc: u64,
+        umi: u64,
+        na: u32,
+        expected_ori: &Strand,
+    ) -> Self {
+        let mut rbuf = [0u8; 255];
+        let mut rec = Self {
+            bc,
+            umi,
+            dirs: Vec::with_capacity(na as usize),
+            refs: Vec::with_capacity(na as usize),
+        };
+
+        for _ in 0..(na as usize) {
+            reader.read_exact(&mut rbuf[0..4]).unwrap();
+            let v = rbuf.pread::<u32>(0).unwrap();
+
+            // fw if the leftmost bit is 1, otherwise rc
+            let strand = if (v & utils::MASK_LOWER_31_U32) > 0 {
+                Strand::Forward
+            } else {
+                Strand::Reverse
+            };
+
+            if expected_ori.same(&strand) || expected_ori.is_unknown() {
+                rec.refs.push(v & utils::MASK_TOP_BIT_U32);
+            }
+        }
+
+        // make sure these are sorted in this step.
+        quickersort::sort(&mut rec.refs[..]);
+        rec
+    }
+
+    pub fn from_bytes_keep_ori<T: Read>(
+        reader: &mut T,
+        bct: &RadIntId,
+        umit: &RadIntId,
+        expected_ori: &Strand,
+    ) -> Self {
+        let mut rbuf = [0u8; 255];
+
+        reader.read_exact(&mut rbuf[0..4]).unwrap();
+        let na = rbuf.pread::<u32>(0).unwrap();
+
+        let bc = read_into_u64(reader, bct);
+        let umi = read_into_u64(reader, umit);
+
+        let mut rec = Self {
+            bc,
+            umi,
+            dirs: Vec::with_capacity(na as usize),
+            refs: Vec::with_capacity(na as usize),
+        };
+
+        for _ in 0..(na as usize) {
+            reader.read_exact(&mut rbuf[0..4]).unwrap();
+            let v = rbuf.pread::<u32>(0).unwrap();
+
+            // fw if the leftmost bit is 1, otherwise rc
+            let strand = if (v & utils::MASK_LOWER_31_U32) > 0 {
+                Strand::Forward
+            } else {
+                Strand::Reverse
+            };
+
+            if expected_ori.same(&strand) || expected_ori.is_unknown() {
+                rec.refs.push(v & utils::MASK_TOP_BIT_U32);
+            }
+        }
+
+        // make sure these are sorted in this step.
+        quickersort::sort(&mut rec.refs[..]);
+        rec
+    }
+}
+
+impl Chunk {
+    pub fn read_header<T: Read>(reader: &mut T) -> (u32, u32) {
+        let mut buf = [0u8; 8];
+
+        reader.read_exact(&mut buf).unwrap();
+        let nbytes = buf.pread::<u32>(0).unwrap();
+        let nrec = buf.pread::<u32>(4).unwrap();
+        (nbytes, nrec)
+    }
+
+    pub fn from_bytes<T: Read>(reader: &mut T, bct: &RadIntId, umit: &RadIntId) -> Self {
+        let mut buf = [0u8; 8];
+
+        reader.read_exact(&mut buf).unwrap();
+        let nbytes = buf.pread::<u32>(0).unwrap();
+        let nrec = buf.pread::<u32>(4).unwrap();
+        let mut c = Self {
+            nbytes,
+            nrec,
+            reads: Vec::with_capacity(nrec as usize),
+        };
+
+        for _ in 0..(nrec as usize) {
+            c.reads.push(ReadRecord::from_bytes(reader, &bct, &umit));
+        }
+
+        c
+    }
+
+    /// peeks to the first record in the buffer `buf`, and returns
+    /// the barcode and umi associated with this record.  It is assumed
+    /// that there is at least one record present in the buffer.
+    pub fn peek_record(buf: &[u8], bct: &RadIntId, umit: &RadIntId) -> (u64, u64) {
+        let na_size = mem::size_of::<u32>();
+        let bc_size = bct.bytes_for_type();
+
+        let _na = buf.pread::<u32>(0).unwrap();
+        let bc;
+        match bct {
+            RadIntId::U8 => {
+                bc = buf.pread::<u8>(na_size).unwrap() as u64;
+            }
+            RadIntId::U16 => {
+                bc = buf.pread::<u16>(na_size).unwrap() as u64;
+            }
+            RadIntId::U32 => {
+                bc = buf.pread::<u32>(na_size).unwrap() as u64;
+            }
+            RadIntId::U64 => {
+                bc = buf.pread::<u64>(na_size).unwrap();
+            }
+        }
+        let umi;
+        match umit {
+            RadIntId::U8 => {
+                umi = buf.pread::<u8>(na_size + bc_size).unwrap() as u64;
+            }
+            RadIntId::U16 => {
+                umi = buf.pread::<u16>(na_size + bc_size).unwrap() as u64;
+            }
+            RadIntId::U32 => {
+                umi = buf.pread::<u32>(na_size + bc_size).unwrap() as u64;
+            }
+            RadIntId::U64 => {
+                umi = buf.pread::<u64>(na_size + bc_size).unwrap();
+            }
+        }
+        (bc, umi)
+    }
+}
+
+impl FileTags {
+    pub fn from_bytes<T: Read>(reader: &mut T) -> Self {
+        let mut buf = [0u8; 4];
+        reader.read_exact(&mut buf).unwrap();
+
+        Self {
+            bclen: buf.pread::<u16>(0).unwrap(),
+            umilen: buf.pread::<u16>(2).unwrap(),
+        }
+    }
+}
+
+impl TagDesc {
+    pub fn from_bytes<T: Read>(reader: &mut T) -> TagDesc {
+        // space for the string length (1 byte)
+        // the longest string possible (255 char)
+        // and the typeid
+        let mut buf = [0u8; 257];
+        reader.read_exact(&mut buf[0..2]).unwrap();
+        let str_len = buf.pread::<u16>(0).unwrap() as usize;
+
+        // read str_len + 1 to get the type id that follows the string
+        reader.read_exact(&mut buf[0..str_len + 1]).unwrap();
+        TagDesc {
+            name: std::str::from_utf8(&buf[0..str_len]).unwrap().to_string(),
+            typeid: buf.pread(str_len).unwrap(),
+        }
+    }
+}
+
+impl TagSection {
+    pub fn from_bytes<T: Read>(reader: &mut T) -> TagSection {
+        let mut buf = [0u8; 2];
+        reader.read_exact(&mut buf).unwrap();
+        let num_tags = buf.pread::<u16>(0).unwrap() as usize;
+
+        let mut ts = TagSection {
+            tags: Vec::with_capacity(num_tags),
+        };
+
+        for _ in 0..num_tags {
+            ts.tags.push(TagDesc::from_bytes(reader));
+        }
+
+        ts
+    }
+}
+
+impl RadHeader {
+    pub fn from_bytes<T: Read>(reader: &mut T) -> RadHeader {
+        let mut rh = RadHeader {
+            is_paired: 0,
+            ref_count: 0,
+            ref_names: vec![],
+            num_chunks: 0,
+        };
+
+        // size of the longest allowable string.
+        let mut buf = [0u8; 65536];
+        reader.read_exact(&mut buf[0..9]).unwrap();
+        rh.is_paired = buf.pread(0).unwrap();
+        rh.ref_count = buf.pread::<u64>(1).unwrap();
+
+        // we know how many names we will read in.
+        rh.ref_names.reserve_exact(rh.ref_count as usize);
+
+        let mut num_read = 0u64;
+        while num_read < rh.ref_count {
+            reader.read_exact(&mut buf[0..2]).unwrap();
+            let l: usize = buf.pread::<u16>(0).unwrap() as usize;
+            reader.read_exact(&mut buf[0..l]).unwrap();
+            rh.ref_names
+                .push(std::str::from_utf8(&buf[0..l]).unwrap().to_string());
+            num_read += 1;
+        }
+
+        reader.read_exact(&mut buf[0..8]).unwrap();
+        rh.num_chunks = buf.pread::<u64>(0).unwrap();
+        rh
+    }
+    pub fn from_bam_header(header: &HeaderView) -> RadHeader {
+        let mut rh = RadHeader {
+            is_paired: 0,
+            ref_count: 0,
+            ref_names: vec![],
+            num_chunks: 0,
+        };
+
+        rh.ref_count = header.target_count() as u64;
+        // we know how many names we will read in.
+        rh.ref_names.reserve_exact(rh.ref_count as usize);
+        for (_i, t) in header
+            .target_names()
+            .iter()
+            .map(|a| std::str::from_utf8(a).unwrap())
+            .enumerate()
+        {
+            rh.ref_names.push(t.to_owned());
+        }
+        rh
+    }
+    pub fn get_size(&self) -> usize {
+        let mut tot_size = 0usize;
+        tot_size += std::mem::size_of::<u8>() + std::mem::size_of::<u64>();
+        for (_i, t) in self.ref_names.iter().map(|a| a.len()).enumerate() {
+            tot_size += t;
+        }
+        tot_size += std::mem::size_of::<u64>();
+        tot_size
+    }
+}
+
+pub fn write_str_bin(v: &str, type_id: &RadIntId, owriter: &mut Cursor<Vec<u8>>) {
+    match type_id {
+        RadIntId::U8 => {
+            owriter
+                .write_all(&(v.len() as u8).to_le_bytes())
+                .expect("coudn't write to output file");
+        }
+        RadIntId::U16 => {
+            owriter
+                .write_all(&(v.len() as u16).to_le_bytes())
+                .expect("coudn't write to output file");
+        }
+        RadIntId::U32 => {
+            owriter
+                .write_all(&(v.len() as u32).to_le_bytes())
+                .expect("coudn't write to output file");
+        }
+        RadIntId::U64 => {
+            owriter
+                .write_all(&(v.len() as u64).to_le_bytes())
+                .expect("coudn't write to output file");
+        }
+    }
+    owriter
+        .write_all(v.as_bytes())
+        .expect("coudn't write to output file");
+}
