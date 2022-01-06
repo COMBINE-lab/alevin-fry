00faaed5edb65980386a1d2b589752818328db64
rob-p <rob.patro@gmail.com>
2022-01-06 11:36:51 -0500

    forgot to add constants file

---
+++ src/constants.rs
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2020-2022 Rob Patro, Avi Srivastava, Hirak Sarkar, Dongze He, Mohsen Zakeri.
+ *
+ * This file is part of alevin-fry
+ * (see https://github.com/COMBINE-lab/alevin-fry).
+ *
+ * License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause
+ */
+
+pub(crate) const PERMIT_FILE_VER: u64 = 1;
